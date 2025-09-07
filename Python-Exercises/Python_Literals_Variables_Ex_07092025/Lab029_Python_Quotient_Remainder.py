64e32699e6247b162a925da0849cf7a3942d5b06
Geeta <geetametri904@gmail.com>
2025-09-07 23:22:59 +0530

    divmod

--- Python-Exercises/Python_Literals_Variables_Ex_07092025/Lab029_Python_Quotient_Remainder.py
+++ Python-Exercises/Python_Literals_Variables_Ex_07092025/Lab029_Python_Quotient_Remainder.py
@@ -13,3 +13,17 @@ Remainder = num1 % num2 # Using modulo to get the remainder
 print("The quotient is", Quotient) # The quotient is 7
 print("The remainder is", Remainder) # The remainder is 1
 
+# python built in function divmod
+# In Python, there is a built-in function called divmod(a, b)
+# It takes two numbers and returns a tuple → (quotient, remainder)
+# divmod() only takes two numbers.
+# It’s different from / (division) because it gives you both quotient and remainder in one go
+
+result = divmod(15, 2)
+print(result)
+
+# output is Because 15 ÷ 2 = 7 (quotient), remainder 1
+# (7, 1)
+
+result_f = divmod(15.5, 2.5)
+print(result_f) # (6.0, 0.5)
