e75d13566b6f30a3c03ac6363c3e51b3a942fd58
Geeta <geetametri904@gmail.com>
2025-09-09 21:17:54 +0530

    Commit_Python_Exercises

---
+++ Python-Exercises/Python_Operators_Ex_08092025/Lab041_Python_Operator_Ex1.py
@@ -0,0 +1,8 @@
+# If x = 2 and y = 5, what is the result of:
+# x + y * 2
+# (x + y) * 2
+
+x = 2
+y = 5
+print( x + y * 2 )  # 12
+print( (x+y) * 2 )  # 14
---
+++ Python-Exercises/Python_Operators_Ex_08092025/Lab042_Python_Operator_Ex2.py
@@ -0,0 +1,9 @@
+# Take three numbers as input and print their average (use /).
+
+num1 = int(input("Enter a first number: "))
+num2 = int(input("Enter a second number: "))
+num3 = int(input("Enter a third number: "))
+
+sum = num1 + num2 + num3
+avg = sum / 3
+print("average is", avg) # 2.0
