2850f5feb5242c8474da6a83144d56727458598a
Geeta <geetametri904@gmail.com>
2025-09-11 22:31:03 +0530

    Commit_Python_Operators

---
+++ Python-Exercises/Python_Operators_Ex_08092025/Lab049_Python_Increment_Decrement.py
@@ -0,0 +1,11 @@
+# Increment (++) / Decrement (--) Operators
+# Good news - ++, -- :)
+x = 5
+x += 1  # x= x+1
+print(x) # 6
+
+x -= 1  # x= x-1
+print(x) # 5
+
+x *= 3  # x= x*1
+print(x) # 15
---
+++ Python-Exercises/Python_Operators_Ex_08092025/Lab051_Python_Example_2.py
@@ -0,0 +1,8 @@
+name = input("Name: ") # Geeta
+age = input("Age: ") # 22
+print(f"Hello {name}, you are {age} years old.") # Hello Geeta, you are 22 years old.
+
+print(" --------------------------- ")
+
+x = int(input("Enter a number: ")) # 2
+print(f"{x} squared is {x ** 2}") # 2 squared is 4
