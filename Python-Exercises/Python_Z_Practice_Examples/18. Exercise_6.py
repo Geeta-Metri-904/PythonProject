b60f90065e0b4ae7d7886992762ef466aa713529
Geeta <geetametri904@gmail.com>
2025-09-26 22:20:29 +0530

    Python_function_Exercises_26

---
+++ Python-Exercises/Python_Z_Practice_Examples/18. Exercise_6.py
@@ -0,0 +1,11 @@
+# Perfect Number
+
+def is_perfect(n):
+    total = 0
+    for i in range(1, n):
+        if n % i == 0:
+            total += i
+    return total == n
+
+print(is_perfect(6))  # True
+print(is_perfect(28)) # True
---
+++ Python-Exercises/Python_Z_Practice_Examples/19. Exercise_7.py
@@ -0,0 +1,6 @@
+# Sum of First n Odd Numbers
+
+def sum_first_n_odd(n):
+    return n * n
+
+print(sum_first_n_odd(5))  # 25
