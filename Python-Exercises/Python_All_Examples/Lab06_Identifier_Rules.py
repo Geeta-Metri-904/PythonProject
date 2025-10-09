07dd05a369f4335ae520af6aeb7b9b645b67c998
Geeta <geetametri904@gmail.com>
2025-10-09 08:11:29 +0530

    Python_Exercises_0910

---
+++ Python-Exercises/Python_All_Examples/Lab06_Identifier_Rules.py
@@ -0,0 +1,26 @@
+# Can contain letters (A–Z, a–z), digits (0–9), and underscores (_)
+#
+# Cannot start with a digit
+#
+# Case-sensitive (age and Age are different)
+#
+# Cannot use Python keywords (like if, while, class, etc.)
+#
+# Should not contain spaces or special characters (@, $, % etc.)
+
+# Valid
+
+name = "Geeta"
+_age = 25
+student1 = "Ravi"
+totalMarks = 450
+_sum = 100
+
+# Invalid
+
+# 1name = "Ravi"      # ❌ starts with a digit
+# my-name = "Geeta"   # ❌ hyphen not allowed
+# class = "Python"    # ❌ keyword
+# total marks = 100   # ❌ space not allowed
+
+
