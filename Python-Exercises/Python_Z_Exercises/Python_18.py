42eda0a9d79b3fcc6082531debe2097d3522081e
Geeta <geetametri904@gmail.com>
2025-10-12 23:04:21 +0530

    Python_Exercises_1012

---
+++ Python-Exercises/Python_Z_Exercises/Python_18.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")  # Remove by value
+fruits.pop(1)            # Remove by index
+del fruits[0]            # Delete element
+fruits.clear()           # Empty the list
---
+++ Python-Exercises/Python_Z_Exercises/Python_20.py
@@ -0,0 +1,5 @@
+numbers = [5, 1, 8, 3]
+print(len(numbers))   # 4
+numbers.sort()        # Sort ascending
+numbers.reverse()     # Reverse order
+print(numbers)
