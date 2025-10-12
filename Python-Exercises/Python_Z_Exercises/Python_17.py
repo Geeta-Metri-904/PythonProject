4bc41ae991038dc10b6ce1a0a48bf57bc43e4878
Geeta <geetametri904@gmail.com>
2025-10-12 23:02:41 +0530

    Python_Exercises_1012

---
+++ Python-Exercises/Python_Z_Exercises/Python_17.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "cherry"]
+fruits[1] = "mango"       # Change element
+fruits.append("orange")   # Add to end
+fruits.insert(1, "kiwi")  # Insert at index
+print(fruits)
