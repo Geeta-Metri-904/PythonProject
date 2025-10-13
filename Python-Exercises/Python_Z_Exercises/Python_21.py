2ceeb7d9d388861834163e2d9459b65da7d2bff8
Geeta <geetametri904@gmail.com>
2025-10-13 22:24:37 +0530

    Python_Exercises_1013

---
+++ Python-Exercises/Python_Z_Exercises/Python_21.py
@@ -0,0 +1,15 @@
+# Creating a tuple with multiple items
+mytuple = ("apple", "banana", "cherry")
+print(mytuple)  # Output: ('apple', 'banana', 'cherry')
+
+# Creating a tuple without parentheses
+mytuple = "apple", "banana", "cherry"
+print(mytuple)
+
+# Creating a tuple with one item (comma needed)
+single_tuple = ("apple",)
+print(type(single_tuple))  # <class 'tuple'>
+
+# Tuple with mixed data types
+mixed_tuple = ("abc", 34, True, 40, "male")
+print(mixed_tuple)
