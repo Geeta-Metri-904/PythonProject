48cb8877192699f9b570a45918861ed4ff16d44d
Geeta <geetametri904@gmail.com>
2025-10-15 23:08:21 +0530

    Python_Exercises_1015_Tuples

---
+++ Python-Exercises/Python_Z_Exercises/Python_22.py
@@ -0,0 +1,10 @@
+mytuple = ("apple", "banana", "cherry")
+print(mytuple)
+# Output: ('apple', 'banana', 'cherry')
+
+tuple1 = ("abc", 34, True, 40, "male")
+print(tuple1)
+# Output: ('abc', 34, True, 40, 'male')
+
+single_item_tuple = ("apple",)
+print(type(single_item_tuple))  # Output: <class 'tuple'>
---
+++ Python-Exercises/Python_Z_Exercises/Python_23.py
@@ -0,0 +1,11 @@
+empty_tuple = ()
+print(empty_tuple)  # Output: ()
+
+tup = 4, 5, 6
+print(tup)
+# Output: (4, 5, 6)
+
+list_data = [7, 8, 9]
+tuple_from_list = tuple(list_data)
+print(tuple_from_list)
+# Output: (7, 8, 9)
