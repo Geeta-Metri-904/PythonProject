f35f264196ff9ea632afeaf10fc856abbbae3d3f
Geeta <geetametri904@gmail.com>
2026-09-06 00:30:29 +0530

    Playwright_Exercises_1

--- Python-Exercises/Playwright-Exercises/test_login_page.py
+++ Python-Exercises/Playwright-Exercises/test_login_page.py
@@ -1,10 +1,17 @@
 import pytest
 import allure
 
-from playwright.sync_api import Page, expect, sync_playwright
+from playwright.sync_api import expect, sync_playwright
 
 def test_login_page():
+    #browser page
     browser = sync_playwright().start().chromium.launch(headless = False)
-    Page = browser.new_page()
-    Page.goto("https:google.com")
-    expect(Page).to_have_title("Google")
+    page = browser.new_page()
+    #code interaction with html web page
+    page.goto("https:google.com")
+
+#Debugging Purpose
+    #breakpoint()
+
+    #validation
+    expect(page).to_have_title("Google")
